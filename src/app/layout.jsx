1fd5b67a7511cc6b76513a5c7a8b0f969bb17da3
sherazi janjua <abubakarsheraz370@gmail.com>
2025-08-28 23:50:24 +0500

    the last commit1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -27,49 +27,57 @@ export default function RootLayout({ children }) {
       >
         {children}
 
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-V1M6W3WDW6"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-V1M6W3WDW6');
-          `}
-        </Script>
+        {/* ✅ Google Analytics */}
+        {process.env.NODE_ENV === "production" && (
+          <>
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-V1M6W3WDW6"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-V1M6W3WDW6');
+              `}
+            </Script>
+          </>
+        )}
 
-        {/* Facebook Pixel */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s){
-              if(f.fbq) return;
-              n=f.fbq=function(){n.callMethod ?
-              n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
-              if(!f._fbq) f._fbq=n;
-              n.push=n; n.loaded=!0; n.version='2.0';
-              n.queue=[]; t=b.createElement(e); t.async=!0;
-              t.src=v; s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)
-            }(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
+        {/* ✅ Facebook Pixel */}
+        {process.env.NODE_ENV === "production" && (
+          <>
+            <Script id="facebook-pixel" strategy="afterInteractive">
+              {`
+                !function(f,b,e,v,n,t,s){
+                  if(f.fbq) return;
+                  n=f.fbq=function(){n.callMethod ?
+                  n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+                  if(!f._fbq) f._fbq=n;
+                  n.push=n; n.loaded=!0; n.version='2.0';
+                  n.queue=[]; t=b.createElement(e); t.async=!0;
+                  t.src=v; s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)
+                }(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
 
-            fbq('init', '1080603874278809'); 
-            fbq('track', 'PageView');
-          `}
-        </Script>
+                fbq('init', '1080603874278809'); 
+                fbq('track', 'PageView');
+              `}
+            </Script>
 
-        {/* NoScript fallback for FB Pixel */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1080603874278809&ev=PageView&noscript=1"
-          />
-        </noscript>
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src="https://www.facebook.com/tr?id=1080603874278809&ev=PageView&noscript=1"
+                alt="fb-pixel"
+              />
+            </noscript>
+          </>
+        )}
       </body>
     </html>
   );
