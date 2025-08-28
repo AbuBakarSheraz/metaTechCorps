0a15c2bbeed114e4d558b1a90ad90c39f54cfb79
sherazi janjua <abubakarsheraz370@gmail.com>
2025-08-28 23:43:18 +0500

    the last commit1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,60 +14,62 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Meta Tech Corps",
-  description: "A leading Digital Marketing Agency Helping Buisness with the power of Artificial Intelligence",
+  description:
+    "A leading Digital Marketing Agency Helping Business with the power of Artificial Intelligence",
 };
 
-  export default function RootLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
-  <html lang="en" suppressHydrationWarning>
-      
-    <body
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning
-    >       
-      {children}
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V1M6W3WDW6"></Script>
-      <Script>
-    {    ` window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-V1M6W3WDW6');`}
-      </Script>
-      <script>
-  !function(f,b,e,v,n,t,s) (
-  if(f.fbq) return;
-  n=f.fbq=function()(    n.callMethod ?
-    n.callMethod.apply(n,arguments) : n.queue.push(arguments)
-  )
-  if(!f._fbq) f._fbq=n;
-  n.push=n;
-  n.loaded=!0;
-  n.version='2.0';
-  n.queue=[];
-  t=b.createElement(e); t.async=!0;
-  t.src=v;
-  s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)
-  )(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
+      >
+        {children}
 
-  fbq('init', '1080603874278809'); 
-  fbq('track', 'PageView');
-  </script>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-V1M6W3WDW6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V1M6W3WDW6');
+          `}
+        </Script>
 
-  <noscript>
-  <img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=1080603874278809&ev=PageView&noscript=1"/>
-  </noscript>
-  <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V1M6W3WDW6"></Script>
-  <Script>
-{  `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
+        {/* Facebook Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq) return;
+              n=f.fbq=function(){n.callMethod ?
+              n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+              if(!f._fbq) f._fbq=n;
+              n.push=n; n.loaded=!0; n.version='2.0';
+              n.queue=[]; t=b.createElement(e); t.async=!0;
+              t.src=v; s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
 
-  gtag('config', 'G-V1M6W3WDW6');`}
-</Script>
+            fbq('init', '1080603874278809'); 
+            fbq('track', 'PageView');
+          `}
+        </Script>
 
-        
+        {/* NoScript fallback for FB Pixel */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1080603874278809&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
